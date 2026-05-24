356496266f710f0785f3a17db9c6e9bdc5cdb19f
Wataru211 <wataru211niko2@gmail.com>
2026-05-24 22:13:09 +0900

    [Critical] Apple JWK をログインリクエスト毎に取得しており可用性リスク

---
+++ backend/src/main/java/backend/auth/apple/AppleJwkConfig.java
@@ -0,0 +1,24 @@
+package backend.auth.apple;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.auth0.jwk.JwkProvider;
+import com.auth0.jwk.JwkProviderBuilder;
+
+@Configuration
+public class AppleJwkConfig {
+    @Bean
+    public JwkProvider appleJwkProvider() throws MalformedURLException {
+        return new JwkProviderBuilder(
+            URI.create("https://appleid.apple.com/auth/keys").toURL()
+        )
+            .cached(10, 24, TimeUnit.HOURS)
+            .rateLimited(10, 1, TimeUnit.MINUTES)
+            .build();
+    }
+}
