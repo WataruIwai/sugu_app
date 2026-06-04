2acf5bb86313d57c7d8c1250f06101169be21299
Wataru211 <wataru211niko2@gmail.com>
2026-06-05 00:12:54 +0900

    AuthServiceとJwtServiceのテストを追加

---
+++ backend/src/test/java/backend/auth/jwt/JwtServiceTest.java
@@ -0,0 +1,41 @@
+package backend.auth.jwt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+
+import backend.exception.UnauthorizedException;
+
+class JwtServiceTest {
+    private static final String JWT_SECRET = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
+
+    @Test
+    void extractUserIdFromGeneratedToken() {
+        JwtService jwtService = new JwtService(JWT_SECRET, Duration.ofHours(24));
+
+        String token = jwtService.generateToken((123L));
+
+        assertEquals(123L, jwtService.extractUserId(token));
+    }
+
+    @Test
+    void throwUnauthorizedWhenTokenIsTampered() {
+        JwtService jwtService = new JwtService(JWT_SECRET, Duration.ofHours(24));
+        String token = jwtService.generateToken(123L);
+
+        String tamperedToken = token.substring(0, token.length() -1) + (token.endsWith("x") ? "y" : "x");
+
+        assertThrows(UnauthorizedException.class, () -> jwtService.extractUserIdFromHeader("Bearer" + tamperedToken));
+    }
+
+    @Test
+    void throwUnauthorizedWhenTokenIsExpired() {
+        JwtService jwtService = new JwtService(JWT_SECRET, Duration.ofSeconds(-1));
+        String token = jwtService.generateToken(123L);
+
+        assertThrows(UnauthorizedException.class, () -> jwtService.extractUserIdFromHeader("Bearer" + token));
+    }
+}
